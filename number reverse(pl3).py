8fc4eeb905dda40fc990a61ba2cabd6e4acd854f
AbunAbi03 <51960067+AbunAbi03@users.noreply.github.com>
2019-07-10 05:50:06 +0530

    print number in reverse order

--- number reverse(pl3).py
+++ number reverse(pl3).py
@@ -1,8 +1,8 @@
-n=int(input())
-t=n
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-print(rev)
+n1=int(input())
+t1=n1
+rev1=0
+while(n1>0):
+    dig=n1%10
+    rev1=rev1*10+dig
+    n1=n1//10
+print(rev1)
