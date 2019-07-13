437b3c6bb84509b481a60e36e4715d5fab108947
AbunAbi03 <51960067+AbunAbi03@users.noreply.github.com>
2019-07-13 10:23:02 +0530

    Using tree segment

---
+++ tree segment (pr12).py
@@ -0,0 +1,13 @@
+num1,q=map(int,input().split())
+val=0
+i=0
+if num1<=100000:
+  
+  arrval=list(map(int,input().split()[:num1]))
+  while(i<q):
+    u,v=map(int,input().split())
+    for j in range(u,v+1):
+      val+=arrval[j]
+    print(val)
+    val=0
+    i+=1
