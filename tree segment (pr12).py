355497bfcd64b396f855cb6183ce1cac772cc164
AbunAbi03 <51960067+AbunAbi03@users.noreply.github.com>
2019-07-13 10:31:21 +0530

    Update tree segment (pr12).py

--- tree segment (pr12).py
+++ tree segment (pr12).py
@@ -6,7 +6,7 @@ if num1<=100000:
   arrval=list(map(int,input().split()[:num1]))
   while(i<q):
     u,v=map(int,input().split())
-    for j in range(u,v+1):
+    for j in range(u-1,v):
       val+=arrval[j]
     print(val)
     val=0
