6e5670579449de910d61d8e7cce82acb33fd6f49
AbunAbi03 <51960067+AbunAbi03@users.noreply.github.com>
2019-07-13 12:54:58 +0530

    Update descending oder(pr15).py

--- descending oder(pr15).py
+++ descending oder(pr15).py
@@ -1,10 +1,7 @@
-num1=int(input())
-if num1<10000:
-  arr2=list(map(int,input().split()))[:num]
-  arr1=[]
-  for r in arr2:
-    ab=bin(r)
-    arr1.append(ab)
-  arr2.sort(reverse=True)
-  for i in arr2:
-    print(i)
+num=input()
+lu=list(map(int,input().split()))
+lu = [bin(xl) for xl in lu]
+lu.sort(reverse=True)
+lu = [int(x,2) for x in lu]
+for xl in lu:
+  print(xl)
